a24dbd79536cc7bf83c10b915b14b1ad2b558318
Denise PC <43567020+Denijar@users.noreply.github.com>
2019-09-03 13:30:05 +1200

    achieved 100% statement coverage for equals() method

---
+++ src/softeng254/coverage/TestStudent.java
@@ -0,0 +1,81 @@
+package softeng254.coverage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestStudent {
+	
+	@Test
+	public void testToString() {
+		Student student3Names = new Student("John", "Doe", "Smith");
+		assertEquals("John Doe Smith", student3Names.toString());
+	}
+	
+	@Test
+	public void testEqualsWhenEqual() {
+		Student student1 = new Student("John", "Doe", "Smith");
+		Student student2 = new Student("John", "Doe", "Smith");
+		assertEquals(student1, student2);
+	}
+	
+	@Test
+	public void testEqualsWhenNull() {
+		Student student1 = new Student("John", "Doe", "Smith");
+		assertNotEquals(student1, null);
+	}
+	
+	@Test
+	public void testEqualsWhenFirstNotEqual() {
+		Student student1 = new Student("Mary", "Doe", "Smith");
+		Student student2 = new Student("John", "Doe", "Smith");
+		assertNotEquals(student1, student2);
+	}
+	
+	@Test
+	public void testEqualsWhenMiddleSizeNotEqual() {
+		List<String> middleList = new ArrayList<>();
+		middleList.add("Sally");
+		middleList.add("Suzy");
+		Student student1 = new Student("John", middleList, "Smith");
+		Student student2 = new Student("John", "Doe", "Smith");
+		assertNotEquals(student1, student2);
+	}
+	
+	@Test
+	public void testEqualsWhenPreferredNotEqual() {
+		Student student1 = new Student("John", "Doe", "Smith");
+		student1.setPreferredName("Not John Doe Smith");
+		Student student2 = new Student("John", "Doe", "Smith");
+		assertNotEquals(student1, student2);
+	}
+	
+	@Test
+	public void testEqualsWhenPreferredEqualsAndLastNotEqual() {
+		Student student1 = new Student("John", "Doe", "James");
+		student1.setPreferredName("John Smith");
+		Student student2 = new Student("John", "Doe", "Smith");
+		student2.setPreferredName("John Smith");
+		assertNotEquals(student1, student2);
+	}
+	
+	@Test
+	public void testEqualsWhenMiddleNamesNotEqual() {
+		List<String> middleList1 = new ArrayList<>();
+		middleList1.add("Sally");
+		middleList1.add("Suzy");
+		Student student1 = new Student("John", middleList1, "Smith");
+		List<String> middleList2 = new ArrayList<>();
+		middleList2.add("Daisy");
+		middleList2.add("Doe");
+		Student student2 = new Student("John", middleList2, "Smith");
+		assertNotEquals(student1, student2);
+	}
+	
+	
+}
